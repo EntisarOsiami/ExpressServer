99fdc62b946ac333398b7c10989fb3b63eb5b55f
Entisar <Code.g.light@gmail.com>
2025-06-16 12:49:37 +0300

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.redirect('/hello-world');
+});
+
+app.get('/hello-world', (req, res) => {
+  res.redirect('/hello-world.json');
+});
+
+app.get('/hello-world.json', (req, res) => {
+  res.send({
+    status: 'success',
+    code: 200,
+    message: 'Hello, World!',
+  });
+});
+
+app.get('/hello-world.jpeg', (req, res) => {
+  res.sendFile(path.join(__dirname, 'hello-world.jpeg'));
+});
+
+
+
+app.get('*', (req, res) => {
+  res.status(404).send(`${req.method} is not supported on ${req.path}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
